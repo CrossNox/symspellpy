c9fd9f1fce5bb6edf6e73761dd0f411b61790b35
ijmermet <ijmermet@gmail.com>
2018-08-16 10:10:26 -0300

    install

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='symspellpy',
+    packages=find_packages(exclude=['test']),
+    package_data={
+        'symspellpy': ['README', 'LICENSE']
+    },
+    version='0.9.0',
+    description='Keyboard layout aware version of SymSpell',
+    long_description=open('README').read(),
+    author='I. Javier Mermet',
+    url='https://github.com/crossnox/symspellpy',
+    keywords=['symspellpy'],
+    install_requires=[
+        'scipy >= 0.19'
+    ],
+    python_requires='>=3.4',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'License :: OSI Approved :: MIT License',
+    ],
+    test_suite="test",
+    entry_points={
+
+    }
+)
